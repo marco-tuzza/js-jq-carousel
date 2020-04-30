789531a7996edb7126145de368c0185b4eba96d1
Marco Tuzza <marcotuzza@hotmail.it>
2020-04-30 18:24:53 +0200

    Click avanti e indietro realizzati

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
+$(".fa-chevron-circle-right").click(
+    function() {
 
+    var visibleImage = $(".active")
+    var nextImage = visibleImage.next()
+    if (nextImage.length) {
+        visibleImage.removeClass('active')
+        nextImage.addClass('active')
+    } else {
+        visibleImage.removeClass('active')
+        $("img:first-child").addClass('active')
+    }
+
+    }
+)
+
+$(".fa-chevron-circle-left").click(
+    function() {
+
+    var visibleImage = $(".active")
+    var prevImage = visibleImage.prev()
+
+    if (prevImage.length) {
+        visibleImage.removeClass('active')
+        prevImage.addClass('active')
+    } else {
+        visibleImage.removeClass('active')
+        $("img:last-child").addClass('active')
+    }
+
+    }
+)
