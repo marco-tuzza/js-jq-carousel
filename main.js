94d491c08bad3c4f5bfc80c85f58852e8660ee40
Marco Tuzza <marcotuzza@hotmail.it>
2020-05-03 16:05:36 +0200

    Pallini slider funzionanti

--- main.js
+++ main.js
@@ -2,13 +2,23 @@ $(".fa-chevron-circle-right").click(
     function() {
 
     var visibleImage = $(".active")
-    var nextImage = visibleImage.next()
+    var nextImage = visibleImage.next("img")
+    var activeCircle = $(".slider-nav > .fas")
+    var nextCircle = activeCircle.next()
+
     if (nextImage.length) {
-        visibleImage.removeClass('active')
-        nextImage.addClass('active')
+        visibleImage.removeClass("active")
+        nextImage.addClass("active")
+        activeCircle.removeClass("fas")
+        activeCircle.addClass ("far")
+        nextCircle.removeClass ("far")
+        nextCircle.addClass ("fas")
     } else {
-        visibleImage.removeClass('active')
-        $("img:first-child").addClass('active')
+        visibleImage.removeClass("active")
+        $("img:first-child").addClass("active")
+        activeCircle.removeClass("fas")
+        activeCircle.addClass ("far")
+        $(".slider-nav > i:first-of-type").addClass("fas")
     }
 
     }
@@ -18,14 +28,25 @@ $(".fa-chevron-circle-left").click(
     function() {
 
     var visibleImage = $(".active")
-    var prevImage = visibleImage.prev()
+    var prevImage = visibleImage.prev("img")
+    var activeCircle = $(".slider-nav > .fas")
+    var prevCircle = activeCircle.prev()
 
     if (prevImage.length) {
-        visibleImage.removeClass('active')
-        prevImage.addClass('active')
+        visibleImage.removeClass("active")
+        prevImage.addClass("active")
+        activeCircle.removeClass("fas")
+        activeCircle.addClass("far")
+        prevCircle.removeClass("far")
+        prevCircle.addClass("fas")
     } else {
-        visibleImage.removeClass('active')
-        $("img:last-child").addClass('active')
+        visibleImage.removeClass("active")
+        $("img:last-of-type").addClass("active")
+        activeCircle.removeClass("fas")
+        activeCircle.addClass("far")
+        $(".slider-nav > i:last-of-type").removeClass("far")
+        $(".slider-nav > i:last-of-type").addClass("fas")
+
     }
 
     }
